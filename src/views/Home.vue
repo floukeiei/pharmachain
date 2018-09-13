efb1f90fe27d1365608363f5d5d8d3ae5258660b
FlouK <flouk.cs07@gmail.com>
2018-09-14 00:52:10 +0700

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Phamachainnnnnnndev"/>
+    <HelloWorld msg="Welcome to Phamachainnnnnnndevv"/>
   </div>
 </template>
 
